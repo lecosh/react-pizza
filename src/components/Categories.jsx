078ee4827a1e2eef6a83570b888d5986962cc4cc
lecosh <artem_vorobey_2001@mail.ru>
2022-10-08 21:10:54 +0300

    add popup, router

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 
 export function Categories({ items, onClickItem }){
-    const [activeItem, setActiveItem] = useState(null)
+    const [activeItem, setActiveItem] = useState(0)
+    function onSelectedItem(index){
+        setActiveItem(index)
+    }
+
     return(
         <div className="categories">
             <ul>
-                <li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>Все</li>
                 {items.map((element, index) => (
                     <li
-                        onClick={() => setActiveItem(index)}
+                        onClick={() => onSelectedItem(index)}
                         className={activeItem === index ? 'active' : ''}
                         key={`${element}_${index}`}>{element}
                     </li>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import { Categories } from './Categories';
 import { Header } from './Header';
+import { SortPopup } from './SortPopup';
 
 export{
     Categories, 
-    Header
+    Header,
+    SortPopup
 }
