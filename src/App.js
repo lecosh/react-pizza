a692b8f2a2d33fb3542662191448d487f4749550
lecosh <artem_vorobey_2001@mail.ru>
2022-10-11 17:10:45 +0300

    add pizzas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import { Routes } from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/db.json')
+    .then(res => res.json())
+    .then((json) => {
+      setPizzas(json.pizzas)
+    })
+  }, [])
+
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route exact path='/' element={<Home />} />
+          <Route exact path='/' element={<Home items={pizzas} />} />
           <Route exact path='/cart' element={<Cart />} />
         </Routes>
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import { Categories } from './Categories';
 import { Header } from './Header';
 import { SortPopup } from './SortPopup';
+import { PizzaBlock } from './PizzaBlock';
 
 export{
     Categories, 
     Header,
-    SortPopup
+    SortPopup,
+    PizzaBlock
 }
